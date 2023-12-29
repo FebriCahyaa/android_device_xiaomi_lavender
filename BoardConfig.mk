6e766d9fb3c537ae35cf462abc732dcbb12e4684
FebriCahyaa <febricahya12345@gmail.com>
2023-12-29 13:55:50 +0700

    lavender: LocalTweaks (2/0)
    * sepolices and commitment are already in this commit.
    * Features:
      Yellow Brightness Control
      Torch Brightness Control
      Haptic Feedback Control
      Display Color Calibration
      Grayscale & KCAL Presets
      Spectrum Profiles
      Dirac/ Sound Enhancer
      Headphone Gain
      Microphone Gain
      Earpiece Gain
      Speaker Gain
      Audio High Performance mode
      Log Tile
      FPS Info
      Doze
      Touch Boost
      CPU Boost
      GPU Boost
      MSM Thermal Control
      LKM Profiles
      TCP Congestion Algorithm
      SELinux Switch
      Backlight Dimmer
    
    * Fix and news sepolices
    Fix and update Spectrum
    Add New Profiles TCP Congestion Algorithm
    Add Cleaner Speaker
    Add Smart Charging
    Add Game Turbo Mode
    Add USB Fasrcharger
    Add BassBooster preset in Dirac
    Add Profile Extreme impuls for CPU in Game Turbo Mode
    Add Translations for PT-BR
    Add HAL Camera L1/L3 QS Tile
    Add LocalTweaks QS Tile
    Add Dirac Sound QS Tile
    values in Spectrum otimized for kernel sdm660 HMP
    Fps Info redenzing and moved for RIGTH
    
    Author: FebriCahyaa <febricahya12345@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,7 +170,6 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/configs/seccomp
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
 
